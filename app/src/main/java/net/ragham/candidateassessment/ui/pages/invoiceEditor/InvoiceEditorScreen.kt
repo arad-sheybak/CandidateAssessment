bd28b5eb4b6de2ffc9b775768218c421ffd007c5
Arad <arad.sheybak@gmail.com>
2026-02-17 13:41:44 +0330

    feat:ui InvoiceEditorScreen and InvoiceItemAddScreen added

--- app/src/main/java/net/ragham/candidateassessment/ui/pages/invoiceEditor/InvoiceEditorScreen.kt
+++ app/src/main/java/net/ragham/candidateassessment/ui/pages/invoiceEditor/InvoiceEditorScreen.kt
@@ -1,8 +1,160 @@
 package net.ragham.candidateassessment.ui.pages.invoiceEditor
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
-fun InvoiceEditorScreen(){
+fun InvoiceEditorScreen() {
+    InvoiceEditorUI()
+}
 
+@Composable
+@Preview
+private fun InvoiceEditorUI() {
+    var sellerName by remember { mutableStateOf("") }
+    var sellerMobile by remember { mutableStateOf("") }
+    var buyerName by remember { mutableStateOf("") }
+    var buyerMobile by remember { mutableStateOf("") }
+
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp)
+            .background(color = Color.White)
+    ) {
+
+        val (sellerSectionTxt,
+            sellerFullNameInput,
+            sellerMobileInput,
+            buyerSectionTxt,
+            buyerFullNameInput,
+            buyerMobileInput,
+            btnAddProduct) = createRefs()
+
+        Text(
+            text = "فروشنده",
+            fontSize = 24.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .constrainAs(sellerSectionTxt) {
+                    top.linkTo(parent.top)
+                    end.linkTo(
+                        parent.end
+                    )
+
+                }
+        )
+
+        OutlinedTextField(
+            value = sellerName,
+            onValueChange = {
+                sellerName = it
+            },
+            label = {
+                Text("نام و نام خانوادگی فروشنده")
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .constrainAs(sellerFullNameInput) {
+                    top.linkTo(sellerSectionTxt.bottom, margin = 8.dp)
+                    end.linkTo(parent.end)
+                }
+
+        )
+
+        OutlinedTextField(
+            value = sellerMobile,
+            onValueChange = {
+                sellerMobile = it
+            },
+            label = { Text("موبایل فروشنده") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            modifier = Modifier
+                .constrainAs(sellerMobileInput) {
+                    top.linkTo(sellerFullNameInput.bottom, margin = 16.dp)
+                    end.linkTo(parent.end)
+
+                }
+        )
+
+        Text(
+            text = "خریدار",
+            fontSize = 24.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .constrainAs(buyerSectionTxt) {
+                    top.linkTo(sellerMobileInput.bottom, margin = 16.dp)
+                    end.linkTo(
+                        parent.end
+                    )
+
+                }
+        )
+
+        OutlinedTextField(
+            value = buyerName,
+            onValueChange = {
+                buyerName = it
+            },
+            label = {
+                Text("نام و نام خانوادگی خریدار")
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .constrainAs(buyerFullNameInput) {
+                    top.linkTo(buyerSectionTxt.bottom, margin = 8.dp)
+                    end.linkTo(parent.end)
+                }
+
+        )
+
+        OutlinedTextField(
+            value = buyerMobile,
+            onValueChange = {
+                buyerMobile = it
+            },
+            label = { Text("موبایل خریدار") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            modifier = Modifier
+                .constrainAs(buyerMobileInput) {
+                    top.linkTo(buyerFullNameInput.bottom, margin = 16.dp)
+                    end.linkTo(parent.end)
+
+                }
+        )
+        Button(onClick = {
+            //navigate to InvoiceItemAddScreen
+            
+        },
+            modifier = Modifier.constrainAs(btnAddProduct){
+                top.linkTo(buyerMobileInput.bottom, margin = 10.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }) {
+            Text("افزودن محصول")
+        }
+    }
 }
--- app/src/main/java/net/ragham/candidateassessment/ui/pages/invoiceItemAddScreen/InvoiceItemAddScreen.kt
+++ app/src/main/java/net/ragham/candidateassessment/ui/pages/invoiceItemAddScreen/InvoiceItemAddScreen.kt
@@ -1,8 +1,101 @@
 package net.ragham.candidateassessment.ui.pages.invoiceItemAddScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun InvoiceItemAddScreen(){
+fun InvoiceItemAddScreen() {
+    InvoiceItemAddUi()
+}
 
+@Composable
+@Preview
+private fun InvoiceItemAddUi() {
+    var productName by remember { mutableStateOf("") }
+    var productUnitPrice by remember { mutableStateOf("") }
+    var productUnit by remember { mutableStateOf("") }
+    var productDiscountAmount by remember { mutableStateOf("") }
+    var productDiscountPercent by remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = productName,
+            onValueChange = {
+                productName = it
+            },
+            label = {
+                Text("نام محصول")
+            }
+        )
+
+        OutlinedTextField(
+            value = productUnitPrice,
+            onValueChange = {
+                productUnitPrice = it
+            },
+            label = {
+                Text("مبلغ واحد محصول")
+            }
+        )
+        OutlinedTextField(
+            value = productUnit,
+            onValueChange = {
+                productUnit = it
+            },
+            label = {
+                Text("واحد")
+            }
+        )
+
+        OutlinedTextField(
+            value = productDiscountAmount,
+            onValueChange = {
+                productDiscountAmount = it
+            },
+            label = {
+                Text("مبلغ تخفیف")
+            }
+        )
+
+        OutlinedTextField(
+            value = productDiscountAmount,
+            onValueChange = {
+                productDiscountAmount = it
+            },
+            label = {
+                Text("درصد تخفیف")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            //navigate to InvoiceEditor
+        }) {
+            Text("ثبت محصول")
+        }
+    }
 }
